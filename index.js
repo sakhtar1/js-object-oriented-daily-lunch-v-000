fc4646e067397add56ac0b218d195937930e524b
Sharmina Akhtar <sakht318@gmail.com>
2018-08-07 09:17:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 // global datastore
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
+
+
+class Neighborhood{
+  constructor(name){
+    this.name = name;
+    this.id = neighborhoodId++;
+  }
+}
